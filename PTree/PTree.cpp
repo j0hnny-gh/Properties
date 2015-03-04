1cd52c859e3e0dc999ea24f816c0c5720364b99c
j0hnny <j0hnny.gmai1@gmail.com>
2015-03-05 01:19:26 +1100

    Working version

--- PTree/PTree.cpp
+++ PTree/PTree.cpp
@@ -20,8 +20,26 @@ struct Vector3
 
 typedef boost::property_tree::ptree tBoostPTree;
 
-struct iProperty
+struct iPropertyIterator
 {
+	virtual ~iPropertyIterator() {}
+
+	virtual bool Next() = 0;
+	virtual class cProperty& Get() = 0;
+};
+
+struct iIterableProperties
+{
+	typedef std::unique_ptr<iPropertyIterator> rUniquePIterator;
+
+	virtual ~iIterableProperties() {}
+
+	virtual rUniquePIterator CreateIterator() const = 0;
+};
+
+class cProperty
+{
+public:
 	enum ePropertyType
 	{
 		ePTString = 0,
@@ -29,72 +47,22 @@ struct iProperty
 		ePTVector3,
 		ePTCollection
 	};
-
-	virtual ~iProperty() {}
-
-	virtual const char* GetName() const = 0;
-	virtual ePropertyType GetType() const = 0;
-
-	template <typename T_>
-	virtual T_ GetValue() const = 0;
-	template <typename T_>
-	virtual void SetValue(const T_& v) = 0;
-};
-
-struct iPropertyIterator
-{
-	virtual ~iPropertyIterator() = 0;
-
-	virtual bool Next() = 0;
-	virtual struct iPVisitorElement& Get() = 0;
-};
-
-struct iIterableProperties
-{
-	typedef std::unique_ptr<iPropertyIterator> tUniquePIterator;
-
-	virtual ~iIterableProperties() = 0;
-
-	virtual tUniquePIterator CreateIterator() const = 0;
-};
-
-struct iPropertyVisitor
-{
-	virtual ~iPropertyVisitor() {}
-
-	template <iProperty::ePropertyType T_>
-	virtual void Visit(iProperty& p) = 0;
-};
-
-struct iPVisitorElement
-{
-	virtual ~iPVisitorElement() {}
-
-	virtual void Accept(iPropertyVisitor& visitor) = 0;
-};
-
-class cProperty : public iProperty, public iPVisitorElement
-{
 public:
-	cProperty(const char* name, int& r) : Name(name), RInt(&r), Type(iProperty::ePTInt) {}
-	cProperty(const char* name, Vector3& r) : Name(name), RVec3(&r), Type(iProperty::ePTVector3) {}
-	cProperty(const char* name, std::string& r) : Name(name), RStr(&r), Type(iProperty::ePTString) {}
-	cProperty(const char* name, const iIterableProperties& r) : Name(name), RCollection(&r), Type(iProperty::ePTCollection) {}
-	virtual ~cProperty() {}
+	cProperty(const char* name, int& r) : Name(name), RInt(&r), Type(cProperty::ePTInt) {}
+	cProperty(const char* name, Vector3& r) : Name(name), RVec3(&r), Type(cProperty::ePTVector3) {}
+	cProperty(const char* name, std::string& r) : Name(name), RStr(&r), Type(cProperty::ePTString) {}
+	cProperty(const char* name, const iIterableProperties& r) : Name(name), RCollection(&r), Type(cProperty::ePTCollection) {}
 
-	// iProperty:
-	virtual const char* GetName() const override { return Name.c_str(); }
-	virtual ePropertyType GetType() const override { return Type; }
+	const char* GetName() const { return Name.c_str(); }
+	ePropertyType GetType() const { return Type; }
 
 	template <typename T_>
-	virtual T_ GetValue() const override;
+	T_ GetValue() const;
 	template <typename T_>
-	virtual void SetValue(const T_& v) override;
-	// iProperty.
+	void SetValue(const T_& v);
 
-	// iPVisitorElement:
-	virtual void Accept(iPropertyVisitor& visitor) override;
-	// iPVisitorElement.
+	template<class V_>
+	void Accept(V_& visitor);
 
 private:
 	const ePropertyType Type;
@@ -110,70 +78,72 @@ private:
 
 template<> int cProperty::GetValue() const
 {
-	assert(Type == iProperty::ePTInt);
+	assert(Type == cProperty::ePTInt);
 	return *RInt;
 }
 
 template<> void cProperty::SetValue(const int& v)
 {
-	assert(Type == iProperty::ePTInt);
+	assert(Type == cProperty::ePTInt);
 	*RInt = v;
 }
 
 template<> const char* cProperty::GetValue() const
 {
-	assert(Type == iProperty::ePTString);
+	assert(Type == cProperty::ePTString);
 	return RStr->c_str();
 }
+
 template<> void cProperty::SetValue(const char* const& v)
 {
-	assert(Type == iProperty::ePTString);
+	assert(Type == cProperty::ePTString);
 	*RStr = v;
 }
 
 template<> const Vector3& cProperty::GetValue() const
 {
-	assert(Type == iProperty::ePTVector3);
+	assert(Type == cProperty::ePTVector3);
 	return *RVec3;
 }
+
 template<> void cProperty::SetValue(const Vector3& v)
 {
-	assert(Type == iProperty::ePTVector3);
+	assert(Type == cProperty::ePTVector3);
 	*RVec3 = v;
 }
 
 template<> const iIterableProperties& cProperty::GetValue() const
 {
-	assert(Type == iProperty::ePTCollection);
+	assert(Type == cProperty::ePTCollection);
 	return *RCollection;
 }
+
 template<> void cProperty::SetValue(const iIterableProperties& v)
 {
-	assert(Type == iProperty::ePTCollection);
+	assert(Type == cProperty::ePTCollection);
 	RCollection = &v;
 }
 
-void cProperty::Accept(iPropertyVisitor& visitor)
+template<class V_>
+void cProperty::Accept(V_& visitor)
 {
 	switch (Type)
 	{
-	case iProperty::ePTInt: visitor.Visit<ePTInt>(*this); break;
-	case iProperty::ePTString: visitor.Visit<ePTString>(*this); break;
-	case iProperty::ePTVector3: visitor.Visit<ePTVector3>(*this); break;
-	case iProperty::ePTCollection: visitor.Visit<ePTCollection>(*this); break;
+	case cProperty::ePTInt: visitor.Visit<ePTInt>(*this); break;
+	case cProperty::ePTString: visitor.Visit<ePTString>(*this); break;
+	case cProperty::ePTVector3: visitor.Visit<ePTVector3>(*this); break;
+	case cProperty::ePTCollection: visitor.Visit<ePTCollection>(*this); break;
 	default: assert(false);
 	}
 }
 
-class cXMLSerializer : public iPropertyVisitor
+class cXMLSerializer
 {
 public:
 	cXMLSerializer(tBoostPTree& pt, iPropertyIterator& iter);
 
-	// iPropertyVisitor:
-	template <iProperty::ePropertyType T_>
-	virtual void Visit(iProperty& p) override;
-	// iPropertyVisitor.
+	template <cProperty::ePropertyType T_>
+	void Visit(cProperty& p);
 
 private:
 	tBoostPTree& PT;
@@ -186,42 +156,40 @@ cXMLSerializer::cXMLSerializer(tBoostPTree& pt, iPropertyIterator& iter)
 		iter.Get().Accept(*this);
 }
 
-template<> void cXMLSerializer::Visit<iProperty::ePTInt>(iProperty& p)
+template<> void cXMLSerializer::Visit<cProperty::ePTInt>(cProperty& p)
 {
 	PT.put<int>(p.GetName(), p.GetValue<int>());
 }
 
-template<> void cXMLSerializer::Visit<iProperty::ePTString>(iProperty& p)
+template<> void cXMLSerializer::Visit<cProperty::ePTString>(cProperty& p)
 {
 	PT.put<std::string>(p.GetName(), p.GetValue<const char*>());
 }
 
-template<> void cXMLSerializer::Visit<iProperty::ePTVector3>(iProperty& p)
+template<> void cXMLSerializer::Visit<cProperty::ePTVector3>(cProperty& p)
 {
 	const Vector3& v = p.GetValue<const Vector3&>();
 	tBoostPTree pt;
 	pt.put<float>("x", v.X);
 	pt.put<float>("y", v.Y);
 	pt.put<float>("z", v.Z);
-	PT.add(p.GetName(), pt);
+	PT.add_child(p.GetName(), pt);
 }
 
-template<> void cXMLSerializer::Visit<iProperty::ePTCollection>(iProperty& p)
+template<> void cXMLSerializer::Visit<cProperty::ePTCollection>(cProperty& p)
 {
 	tBoostPTree pt;
 	cXMLSerializer(pt, *p.GetValue<const iIterableProperties&>().CreateIterator());
-	PT.add(p.GetName(), pt);
+	PT.add_child(p.GetName(), pt);
 }
 
-class cXMLDeserializer : public iPropertyVisitor
+class cXMLDeserializer
 {
 public:
 	cXMLDeserializer(tBoostPTree& pt, iPropertyIterator& iter);
 
-	// iPropertyVisitor:
-	template <iProperty::ePropertyType T_>
-	virtual void Visit(iProperty& p) override;
-	// iPropertyVisitor.
+	template <cProperty::ePropertyType T_>
+	void Visit(cProperty& p);
 
 private:
 	tBoostPTree& PT;
@@ -234,38 +202,42 @@ cXMLDeserializer::cXMLDeserializer(tBoostPTree& pt, iPropertyIterator& iter)
 		iter.Get().Accept(*this);
 }
 
-template<> void cXMLDeserializer::Visit<iProperty::ePTInt>(iProperty& p)
+template<> void cXMLDeserializer::Visit<cProperty::ePTInt>(cProperty& p)
 {
 	p.SetValue(PT.get<int>(p.GetName()));
 }
 
-template<> void cXMLDeserializer::Visit<iProperty::ePTString>(iProperty& p)
+template<> void cXMLDeserializer::Visit<cProperty::ePTString>(cProperty& p)
 {
-	p.SetValue(PT.get<std::string>(p.GetName()));
+	p.SetValue(PT.get<std::string>(p.GetName()).c_str());
 }
 
-template<> void cXMLDeserializer::Visit<iProperty::ePTVector3>(iProperty& p)
+template<> void cXMLDeserializer::Visit<cProperty::ePTVector3>(cProperty& p)
 {
-	tBoostPTree pt = PT.get<tBoostPTree>(p.GetName());
+	tBoostPTree pt = PT.get_child(p.GetName());
 	p.SetValue(Vector3(pt.get<float>("x"), pt.get<float>("y"), pt.get<float>("z")));
 }
 
-template<> void cXMLDeserializer::Visit<iProperty::ePTCollection>(iProperty& p)
+template<> void cXMLDeserializer::Visit<cProperty::ePTCollection>(cProperty& p)
 {
-	tBoostPTree pt = PT.get<tBoostPTree>(p.GetName());
+	tBoostPTree pt = PT.get_child(p.GetName());
 	cXMLDeserializer(pt, *p.GetValue<const iIterableProperties&>().CreateIterator());
 }
 
 class cPropertiesList : public iIterableProperties
 {
 public:
-	typedef std::unique_ptr<cProperty> rProperty;
+	typedef cProperty* rProperty;
 
 public:
-	cPropertiesList();
+	~cPropertiesList()
+	{
+		for (auto& p : Properties)
+			delete p;
+	}
 
 	// iIterableProperties:
-	virtual tUniquePIterator CreateIterator() const override { return std::unique_ptr<iPropertyIterator>(new cIterator(Properties)); }
+	virtual rUniquePIterator CreateIterator() const override { return rUniquePIterator(new cIterator(Properties)); }
 	// iIterableProperties.
 
 	void Register(rProperty p) { Properties.push_back(p); }
@@ -278,8 +250,8 @@ private:
 	public:
 		cIterator(const tProperties& properties) : Properties(properties), Index(-1) {}
 		// iPropertyIterator:
-		virtual bool Next() override { return ++Index < Properties.size(); }
-		virtual iPVisitorElement& Get() override { assert(Index < Properties.size()); return *Properties[Index].get(); }
+		virtual bool Next() override { return ++Index < (int)Properties.size(); }
+		virtual cProperty& Get() override { assert(Index < (int)Properties.size()); return *Properties[Index]; }
 		// iPropertyIterator.
 
 	private:
@@ -312,16 +284,32 @@ protected:
 class cPropertiesSystem
 {
 public:
-	void Register(iBaseProperties& object) { Registery.push_back(object); }
+	void Register(iBaseProperties& object) { Registery.push_back(&object); }
 	void SaveXML(const char* file);
-	void LoadXML(const char* file) {}
+	void LoadXML(const char* file);
 
 private:
-	typedef std::vector<iBaseProperties&> tRegistry;
+	typedef std::vector<iBaseProperties*> tRegistry;
 	tRegistry Registery;
 };
 
-class cTestActor : cBaseProperties
+void cPropertiesSystem::SaveXML(const char* file)
+{
+	tBoostPTree pt;
+	for (auto& obj : Registery)
+		cXMLSerializer saver(pt, *obj->GetProperties().CreateIterator());
+	write_xml(file, pt);
+}
+
+void cPropertiesSystem::LoadXML(const char* file)
+{
+	tBoostPTree pt;
+	read_xml(file, pt);
+	for (auto& obj : Registery)
+		cXMLDeserializer loader(pt, *obj->GetProperties().CreateIterator());
+}
+
+class cTestActor : public cBaseProperties
 {
 public:
 	cTestActor()
@@ -329,9 +317,9 @@ public:
 		, Health(100)
 		, Pos(100.f, 50.f, 0.f)
 	{
-		PList.Register(std::unique_ptr<cProperty>(new cProperty("Name", Name)));
-		PList.Register(std::unique_ptr<cProperty>(new cProperty("Health", Health)));
-		PList.Register(std::unique_ptr<cProperty>(new cProperty("Position", Pos)));
+		PList.Register(cPropertiesList::rProperty(new cProperty("Name", Name)));
+		PList.Register(cPropertiesList::rProperty(new cProperty("Health", Health)));
+		PList.Register(cPropertiesList::rProperty(new cProperty("Position", Pos)));
 	}
 	~cTestActor() {}
 
@@ -343,5 +331,10 @@ private:
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	cPropertiesSystem propSystem;
+	cTestActor actor;
+	propSystem.Register(actor);
+	propSystem.LoadXML("termogoyf");
+	propSystem.SaveXML("termogoyf.xml");
 	return 0;
 }
